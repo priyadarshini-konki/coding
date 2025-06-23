e23a93f954f9d972014fa1c75d2f0581df59629a
konki priyadharshini <priyadarshinikonki@gmail.com>
2025-06-23 19:47:32 +0530

    Create pattern3.cpp

---
+++ pattern3.cpp
@@ -0,0 +1,15 @@
+void pattern3(int n){
+for (int i=1; i<=n; i++){
+  for (int j=1; j<=i; j++){
+    cout << j;
+  }
+  cout << endl;
+}
+/* output:
+//n=5
+    1
+    12
+    123
+    1234
+    12345
+*/

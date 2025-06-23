aeeba48bd422454a81cc5d323f877ddf0e2dda32
konki priyadharshini <priyadarshinikonki@gmail.com>
2025-06-23 19:37:08 +0530

    Create patterns1.cpp

---
+++ patterns1.cpp
@@ -0,0 +1,31 @@
+//patterns
+
+void pattern1(int n){
+for (int i=0; i<n; i++){
+  for (int j=0; j<n; j++){
+    cout << "*";
+  }
+  cout << endl;
+}
+/* output:
+    ****
+    ****
+    ****
+    **** */
+
+int main(){
+  int n;
+  cin >> n;
+  pattern1(n);
+}
+
+/* for test cases 
+int main(){
+  int t;
+  cin >> t;
+  for ( int i=0 ; i<t ; i++ ){
+  cin >> n;
+  pattern1(n);
+  }
+}
+*/

f0001aba0586bd52a5bfcf45d76ef9a05a57a344
konki priyadharshini <priyadarshinikonki@gmail.com>
2025-06-23 20:04:04 +0530

    rename patterns1.cpp to pattern1.cpp

--- pattern1.cpp
+++ pattern1.cpp
@@ -1,11 +1,14 @@
 //patterns
+#include<iostream>
+using namespace std;
 
 void pattern1(int n){
 for (int i=0; i<n; i++){
   for (int j=0; j<n; j++){
     cout << "*";
-  }
+      }
   cout << endl;
+  }
 }
 /* output:
     ****
